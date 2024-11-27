f226b3adeb3456bb955254b9446e91aa9b147403
p-mykhailenko <p.mykhailenko@creatio.com>
2024-11-27 23:36:57 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+  return {
+    name: 'John Doe',
+    age: 25,
+    skills: {
+      frontend: ['HTML', 'CSS', 'JavaScript'],
+      backend: ['Node.js', 'Express.js', 'MongoDB'],
+    },
+  };
+}
